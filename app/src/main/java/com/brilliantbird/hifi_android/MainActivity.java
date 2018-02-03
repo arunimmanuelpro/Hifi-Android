316b8615fb3c40b832b32175318d1015489699de
Rajaprithya <rajaprithya98@gmail.com>
2018-02-03 13:18:43 +0530

    added log message

--- app/src/main/java/com/brilliantbird/hifi_android/MainActivity.java
+++ app/src/main/java/com/brilliantbird/hifi_android/MainActivity.java
@@ -2,6 +2,7 @@ package com.brilliantbird.hifi_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.brilliantbird.hifi_android.exception.CustomException;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("status","application started");
         throw new CustomException("Intentionally thrown");
     }
 }
